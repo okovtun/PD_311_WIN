76c56720fa5a2a1c492e9b854596233b186f5062
User <User@DESKTOP-0TCDSR6>
2024-07-29 15:27:14 +0300

    Copy text DONE

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -38,11 +38,26 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON_BTC));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+//		SendMessage(окно, сообщение, параметры сообщения)
+
+		/*HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SetFocus(hEditLogin);*/
+		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
 	}
 	break;
 	case WM_COMMAND:	//Здесь обраьатываются нажатия на кнопки, ввод текста, и любые изменения состояния окна.
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+			break;
 		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL:	EndDialog(hwnd, 0);
 		}
--- WinAPI/WinAPI/resource.h
+++ WinAPI/WinAPI/resource.h
@@ -3,8 +3,13 @@
 // Used by Resource.rc
 //
 #define IDD_DIALOG1                     101
-#define IDI_ICON1                       103
 #define IDI_ICON_BTC                    103
+#define IDC_STATIC_LOGIN                1002
+#define IDC_STATIC_PASSWORD             1003
+#define IDC_EDIT_LOGIN                  1004
+#define IDC_EDIT3                       1005
+#define IDC_EDIT_PASSWORD               1005
+#define IDC_BUTTON_COPY                 1007
 
 // Next default values for new objects
 // 
@@ -12,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
