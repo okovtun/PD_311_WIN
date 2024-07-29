46b91481032c19fd1d96f97d45a6e9e4b592165b
User <User@DESKTOP-0TCDSR6>
2024-07-29 18:39:04 +0300

    ComboBox in progress

---
+++ WinAPI/ComboBox/main.cpp
@@ -0,0 +1,34 @@
+#include<Windows.h>
+#include"resource.h"
+
+CONST CHAR* values[] = { "This", "is", "my", "first", "Combo", "Box" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+		for (int i = 0; i < sizeof(values) / sizeof(values[0]); i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)values[i]);
+		}
+	}
+		break;
+
+	case WM_COMMAND:
+		break;
+
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
