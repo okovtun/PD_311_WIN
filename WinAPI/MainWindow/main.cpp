88434e2fef38df018965ffd68be529b5ed73e85a
User <User@DESKTOP-0TCDSR6>
2024-08-19 18:03:59 +0300

    Cursors

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,6 +1,17 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
+//#define TEXT_ELEMENTS
+#ifdef TEXT_ELEMENTS
+#define IDC_STATIC_TEXT		1000
+#define IDC_EDIT			1001
+#define IDC_BUTTON			1002  
+#endif // TEXT_ELEMENTS
+
+#define IDC_STATIC	1000
+#define IDC_COMBO	1001
+#define IDC_BUTTON	1002
+
 /*
 System Classes:
 	"Button", "Edit", "ComboBox", "ListBox", "Static", "MDIClient" (Multi-Document Interface)
@@ -30,11 +41,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wc.cbClsExtra = 0;
 
 	//1.2) Внешний вид:
-	wc.hIcon = (HICON)LoadImage(hInstance, "palm.ico", IMAGE_ICON, LR_DEFAULTSIZE,LR_DEFAULTSIZE, LR_LOADFROMFILE);
-	wc.hIconSm = (HICON)LoadImage(hInstance, "shell.ico", IMAGE_ICON, LR_DEFAULTSIZE,LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hIcon = (HICON)LoadImage(hInstance, "palm.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hIconSm = (HICON)LoadImage(hInstance, "shell.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	//wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	//wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = (HCURSOR)LoadImage(hInstance, "starcraft-original\\Background.ani", IMAGE_CURSOR, LR_DEFAULTSIZE,LR_DEFAULTSIZE,LR_LOADFROMFILE);
+	wc.hCursor = (HCURSOR)LoadImage(hInstance, "starcraft-original\\Background.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	//wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
@@ -90,18 +101,142 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	return 0;
 }
 
+
+
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
 	case WM_CREATE:
+	{
+#ifdef TEXT_ELEMENTS
+		HWND hStatic = CreateWindowEx
+		(
+			NULL,		//EX_STYLE
+			"Static",	//WindowClass
+			"This is static text",	//WindowName - WindowTitle
+			WS_CHILDWINDOW | WS_VISIBLE,	//Style
+			100, 10,	//Position
+			200, 20,	//Dimensions
+			hwnd,		//Parent Window
+			(HMENU)IDC_STATIC_TEXT,//ResourceID
+			NULL,	//GetModuleHandle(NULL)	если оставить 'NULL', то возтмет 'hInstance' родительского окна
+			NULL
+		);
+		HWND hEdit = CreateWindowEx
+		(
+			NULL, "Edit", "Введите текст",
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_CENTER,
+			100, 30,
+			200, 20,
+			hwnd,
+			(HMENU)IDC_EDIT,
+			NULL,
+			NULL
+		);
+		HWND hButton = CreateWindowEx
+		(
+			NULL, "Button", "Применить",
+			WS_CHILD | WS_VISIBLE,
+			200, 55,
+			100, 32,
+			hwnd,
+			(HMENU)IDC_BUTTON,
+			NULL,
+			NULL
+		);
+#endif // TEXT_ELEMENTS
+
+		HWND hStatic = CreateWindowEx(
+			NULL, "Static", "Выберите указатель мыши:",
+			WS_CHILD | WS_VISIBLE,
+			10, 10,
+			200, 20,
+			hwnd,
+			(HMENU)IDC_STATIC,
+			NULL,
+			NULL
+		);
+		HWND hCombo = CreateWindowEx(
+			NULL, "ComboBox", "",
+			WS_CHILD | WS_VISIBLE | WS_VSCROLL | CBS_DROPDOWNLIST,
+			10, 25,
+			200, 200,
+			hwnd,
+			(HMENU)IDC_COMBO,
+			NULL,
+			NULL
+		);
+		HWND hButton = CreateWindowEx(
+			NULL, "Button", "Применить",
+			WS_CHILD | WS_VISIBLE,
+			110, 50,
+			100, 28,
+			hwnd,
+			(HMENU)IDC_BUTTON,
+			NULL,
+			NULL
+		);
+
+		CHAR sz_current_dir[MAX_PATH]{};
+		SetCurrentDirectory("starcraft-original");
+		GetCurrentDirectory(MAX_PATH, sz_current_dir);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_current_dir);
+
+		WIN32_FIND_DATA file_data;
+		ZeroMemory(&file_data, sizeof(file_data));
+		HANDLE hFile = FindFirstFile("*", &file_data);
+		for (; FindNextFile(hFile, &file_data);)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)file_data.cFileName);
+		}
+		FindClose(hFile);
+	}
 		break;
 	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+#ifdef TEXT_ELEMENTS
+		case IDC_BUTTON:
+		{
+			HWND hStaticText = GetDlgItem(hwnd, IDC_STATIC_TEXT);
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hStaticText, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+#endif // TEXT_
+		case IDC_BUTTON:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			CHAR sz_selected_cursor[_MAX_FNAME]{};
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_selected_cursor);
+			HCURSOR hCursor = (HCURSOR)LoadImage(NULL, sz_selected_cursor, IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+			SetCursorPos(300, 300);
+			SendMessage(hwnd, WM_SETCURSOR, 0, (LPARAM)sz_selected_cursor);
+			//SetCursor(hCursor);
+			return FALSE;//https://stackoverflow.com/questions/19257237/reset-cursor-in-wm-setcursor-handler-properly
+		}
+			break;
+		}
+	}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
 	case WM_CLOSE:
+		/*switch (MessageBox(hwnd, "Вы действительно хотите закрыть окно,", "Че, серьезно?", MB_YESNO | MB_ICONQUESTION))
+		{
+		case IDYES:
+			MessageBox(hwnd, "Лучше двери закройте", "Info", MB_OK | MB_ICONINFORMATION);
+			DestroyWindow(hwnd);
+		case IDNO:break;
+		}*/
 		DestroyWindow(hwnd);
 		break;
 	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
