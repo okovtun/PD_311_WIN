9a061b2f16cce24bac0be3d6e6fc0b82d897bbda
User <User@DESKTOP-0TCDSR6>
2024-08-30 16:28:28 +0300

    CalcResourceDLL

---
+++ WinAPI/CalcResourceDLL/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+
+#define IDI_ICON1						 16
+
+#define IDB_BUTTON_0                    100
+#define IDB_BUTTON_1                    101
+#define IDB_BUTTON_2                    102
+#define IDB_BUTTON_3                    103
+#define IDB_BUTTON_4                    104
+#define IDB_BUTTON_5                    105
+#define IDB_BUTTON_6                    106
+#define IDB_BUTTON_7                    107
+#define IDB_BUTTON_8                    108
+#define IDB_BUTTON_9                    109
+#define IDB_BUTTON_POINT                110
+#define IDB_BUTTON_PLUS                 111
+#define IDB_BUTTON_MINUS                112
+#define IDB_BUTTON_ASTER                113
+#define IDB_BUTTON_SLASH                114
+#define IDB_BUTTON_BSP                  115
+#define IDB_BUTTON_CLEAR                116
+#define IDB_BUTTON_EQUAL                117
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        120
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
