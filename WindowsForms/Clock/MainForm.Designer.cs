e4a95135ee0ccb5a270451b0df58b686cc5e61b7
User <User@DESKTOP-0TCDSR6>
2024-09-18 15:37:13 +0300

    Clock in progress

--- WindowsForms/Clock/MainForm.Designer.cs
+++ WindowsForms/Clock/MainForm.Designer.cs
@@ -29,20 +29,31 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			this.label1 = new System.Windows.Forms.Label();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.labelTime = new System.Windows.Forms.Label();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.cbShowDate = new System.Windows.Forms.CheckBox();
+			this.btnHideControls = new System.Windows.Forms.Button();
+			this.notifyIconSystemTray = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.showControlsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.showDateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// label1
+			// labelTime
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(27, 25);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(144, 51);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "label1";
+			this.labelTime.AutoSize = true;
+			this.labelTime.ContextMenuStrip = this.contextMenuStrip1;
+			this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.labelTime.Location = new System.Drawing.Point(27, 25);
+			this.labelTime.Name = "labelTime";
+			this.labelTime.Size = new System.Drawing.Size(144, 51);
+			this.labelTime.TabIndex = 0;
+			this.labelTime.Text = "label1";
+			//this.labelTime.MouseHover += new System.EventHandler(this.labelTime_MouseHover);
 			// 
 			// timer1
 			// 
@@ -54,22 +65,84 @@
 			// 
 			this.cbShowDate.AutoSize = true;
 			this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.cbShowDate.Location = new System.Drawing.Point(36, 183);
+			this.cbShowDate.Location = new System.Drawing.Point(36, 142);
 			this.cbShowDate.Name = "cbShowDate";
 			this.cbShowDate.Size = new System.Drawing.Size(245, 41);
 			this.cbShowDate.TabIndex = 1;
 			this.cbShowDate.Text = "Показать дату";
 			this.cbShowDate.UseVisualStyleBackColor = true;
+			this.cbShowDate.CheckedChanged += new System.EventHandler(this.cbShowDate_CheckedChanged);
+			// 
+			// btnHideControls
+			// 
+			this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btnHideControls.Location = new System.Drawing.Point(36, 230);
+			this.btnHideControls.Name = "btnHideControls";
+			this.btnHideControls.Size = new System.Drawing.Size(227, 55);
+			this.btnHideControls.TabIndex = 2;
+			this.btnHideControls.Text = "Hide controls";
+			this.btnHideControls.UseVisualStyleBackColor = true;
+			this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+			// 
+			// notifyIconSystemTray
+			// 
+			this.notifyIconSystemTray.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIconSystemTray.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconSystemTray.Icon")));
+			this.notifyIconSystemTray.Text = "Clock";
+			this.notifyIconSystemTray.Visible = true;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showControlsToolStripMenuItem,
+            this.showDateToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(150, 76);
+			// 
+			// showControlsToolStripMenuItem
+			// 
+			this.showControlsToolStripMenuItem.Checked = true;
+			this.showControlsToolStripMenuItem.CheckOnClick = true;
+			this.showControlsToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.showControlsToolStripMenuItem.Name = "showControlsToolStripMenuItem";
+			this.showControlsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.showControlsToolStripMenuItem.Text = "Show controls";
+			this.showControlsToolStripMenuItem.Click += new System.EventHandler(this.showControlsToolStripMenuItem_Click);
+			// 
+			// showDateToolStripMenuItem
+			// 
+			this.showDateToolStripMenuItem.CheckOnClick = true;
+			this.showDateToolStripMenuItem.Name = "showDateToolStripMenuItem";
+			this.showDateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.showDateToolStripMenuItem.Text = "Show date";
+			this.showDateToolStripMenuItem.Click += new System.EventHandler(this.showDateToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+			// 
+			// quitToolStripMenuItem
+			// 
+			this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+			this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.quitToolStripMenuItem.Text = "Quit";
+			this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(667, 450);
+			this.ClientSize = new System.Drawing.Size(522, 450);
+			this.Controls.Add(this.btnHideControls);
 			this.Controls.Add(this.cbShowDate);
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.labelTime);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
 			this.Text = "MainForm";
+			this.contextMenuStrip1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -77,9 +150,16 @@
 
 		#endregion
 
-		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label labelTime;
 		private System.Windows.Forms.Timer timer1;
 		private System.Windows.Forms.CheckBox cbShowDate;
+		private System.Windows.Forms.Button btnHideControls;
+		private System.Windows.Forms.NotifyIcon notifyIconSystemTray;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem showControlsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem showDateToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
 	}
 }
 
--- WindowsForms/Clock/MainForm.cs
+++ WindowsForms/Clock/MainForm.cs
@@ -12,15 +12,54 @@ namespace Clock
 {
 	public partial class MainForm : Form
 	{
+		bool controlsVisible;
 		public MainForm()
 		{
 			InitializeComponent();
+			controlsVisible = true;
 		}
 
 		private void timer1_Tick(object sender, EventArgs e)
 		{
-			label1.Text = DateTime.Now.ToString("hh:mm:ss tt");
-			if (cbShowDate.Checked) label1.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+			labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
+			if (cbShowDate.Checked) labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+		}
+
+		private void btnHideControls_Click(object sender, EventArgs e)
+		{
+			SetControlsVisibility(false);
+		}
+		void SetControlsVisibility(bool visible)
+		{
+			this.FormBorderStyle = visible ? FormBorderStyle.Sizable : FormBorderStyle.None;
+			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
+			this.ShowInTaskbar = visible;
+			this.cbShowDate.Visible = visible;
+			this.TopMost = !visible;
+			btnHideControls.Visible = visible;
+			labelTime.BackColor = visible ? this.BackColor : Color.LightBlue;
+			showControlsToolStripMenuItem.Checked = visible;
+		}
+
+		private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void showDateToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			cbShowDate.Checked = showDateToolStripMenuItem.Checked;
+		}
+
+		private void cbShowDate_CheckedChanged(object sender, EventArgs e)
+		{
+			showDateToolStripMenuItem.Checked = cbShowDate.Checked;
+		}
+
+		private void showControlsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.controlsVisible = showControlsToolStripMenuItem.Checked;
+			SetControlsVisibility(controlsVisible);
 		}
 	}
 }
